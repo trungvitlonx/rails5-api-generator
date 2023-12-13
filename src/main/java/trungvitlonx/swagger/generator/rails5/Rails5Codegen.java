5afdb4b63e81ad4711f3b4e13df893677d0c2d56
trung-csv <trung.lq@cs.ventures>
2023-12-13 12:47:44 +0700

    init project

---
+++ src/main/java/trungvitlonx/swagger/generator/rails5/Rails5Codegen.java
@@ -0,0 +1,26 @@
+package trungvitlonx.swagger.generator.rails5;
+
+import io.swagger.codegen.v3.CodegenType;
+import io.swagger.codegen.v3.generators.DefaultCodegenConfig;
+
+public class Rails5Codegen extends DefaultCodegenConfig {
+    @Override
+    public String getDefaultTemplateDir() {
+        return null;
+    }
+
+    @Override
+    public CodegenType getTag() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getHelp() {
+        return null;
+    }
+}
